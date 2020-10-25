d8821f24752fc62d03962c9e3921ed44690d66f9
Dylan Karapekmez <dylankarapekmez@icloud.com>
2020-10-25 16:19:34 +0100

    first commit

---
+++ src/main/java/be/ehb/generatieservice/GeneratieserviceApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.generatieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratieserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratieserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/generatieservice/GeneratieserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.generatieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratieserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
